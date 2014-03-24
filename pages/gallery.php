0ab3fe2fc328cbb7964983e293327aa95e7f5df3
Michel Albert <michel@albert.lu>
2014-03-24 18:31:40 +0100

    Show error if no photos are found.

--- pages/gallery.php
+++ pages/gallery.php
@@ -6,15 +6,21 @@ $f = new phpFlickr(trim(file_get_contents('flickr_key.txt')));
 
 $recent = $f->photos_search(array("tags" => "mamerwiselen:event=lost2013"));
 
-foreach ($recent['photo'] as $photo) {
-    $owner = $f->people_getInfo($photo['owner']);
-    printf("<a href=\"http://farm%s.staticflickr.com/%s/%s_%s.jpg\"><img src=\"http://farm%s.staticflickr.com/%s/%s_%s_q.jpg\" /></a>",
-      $photo['farm'],
-      $photo['server'],
-      $photo['id'],
-      $photo['secret'],
-      $photo['farm'],
-      $photo['server'],
-      $photo['id'],
-      $photo['secret']);
+if ($recent['total'] == 0) {
+  ?>
+  <p class="note">Hei ass nach näischt!</p>
+  <?php
+} else {
+  foreach ($recent['photo'] as $photo) {
+      $owner = $f->people_getInfo($photo['owner']);
+      printf("<a href=\"http://farm%s.staticflickr.com/%s/%s_%s.jpg\"><img src=\"http://farm%s.staticflickr.com/%s/%s_%s_q.jpg\" /></a>",
+        $photo['farm'],
+        $photo['server'],
+        $photo['id'],
+        $photo['secret'],
+        $photo['farm'],
+        $photo['server'],
+        $photo['id'],
+        $photo['secret']);
+  }
 }
